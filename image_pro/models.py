cda6da5f4417457f594ab7a2afbc37ff8a32b866
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-02-26 11:22:27 +0100

    feat: add status to image  model

--- image_pro/models.py
+++ image_pro/models.py
@@ -7,24 +7,32 @@ User = get_user_model()
 
 
 def default_expiry():
-    return timezone.now() + timedelta(days=1)
+    return timezone.now() + timedelta(minutes=5)
 class Image(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "Pending"),
+        ("processing", "Processing"),
+        ("completed", "Completed"),
+        ("failed", "Failed"),
+    )
+
     IMAGE_FORMAT_CHOICES = (
         ("jpg", "JPEG"),
         ("png", "PNG"),
         ("webp", "WebP")
     )
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="images")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="images", null=True, blank=True)
     original_image = models.ImageField(upload_to="images/originals/")
     processed_image = models.ImageField(upload_to="images/processed/", null=True, blank=True)
-    image_format = models.CharField(max_length=4, choices=IMAGE_FORMAT_CHOICES, default="jpg")
+    image_format = models.CharField(max_length=4, choices=IMAGE_FORMAT_CHOICES)
     is_anonymous = models.BooleanField(default=False)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
     created_at = models.DateTimeField(auto_now_add=True)
-    expires_at = models.DateTimeField(default=default_expiry)
+    download_expires_at = models.DateTimeField(default=default_expiry)
 
     def __str__(self):
-        return f"Image {self.id} - {self.user.username}"
+        return f"Image {self.id}"
 
 
 
