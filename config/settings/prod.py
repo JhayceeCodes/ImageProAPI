a34f954a75783cd4e0498cb33ed5c38ae12246ad
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-03-06 00:46:26 +0100

    feat: implement rate limiting

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -63,3 +63,21 @@ STORAGES = {
     },
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.getenv("REDIS_URL", "redis://127.0.0.1:6379/1"),
+        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
+    }
+}
+
+REST_FRAMEWORK.update({
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.UserRateThrottle",
+        "rest_framework.throttling.AnonRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "user": "20/min",
+        "anon": "5/min",
+    }
+})
