00b0662c5829cc26e6114c11e775faf5e09c12bf
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-03-06 03:33:26 +0100

    fix: modify storages to disable acl

--- config/storages.py
+++ config/storages.py
@@ -1,13 +1,12 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
-
 class StaticStorage(S3Boto3Storage):
     location = "static"
-    default_acl = "public-read"
+    default_acl=None
     querystring_auth = False
-
 
 class MediaStorage(S3Boto3Storage):
     location = "media"
-    default_acl = "private"
+    default_acl=None
     file_overwrite = False
+    querystring_auth = True  
