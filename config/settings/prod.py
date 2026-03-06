da31b89a9fe884e1a78eb3da754f52fea19dcee2
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-03-06 05:03:53 +0100

    feat: temporarily add logging feature in production settings

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -84,3 +84,17 @@ REST_FRAMEWORK.update({
         "anon": "5/min",
     }
 })
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
